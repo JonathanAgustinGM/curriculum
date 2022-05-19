cb0a15425fa62f4c141d5f55d40d0a386c9d6115
Jonathan Gracia <jonathanagustin@ciencias.unam.mx>
2022-05-18 23:14:16 -0500

    Initial Commit

---
+++ CurriculimVitae/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurriculimVitae
+//
+//  Created by Jordy Gracia on 18/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
